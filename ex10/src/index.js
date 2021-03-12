d2c3f47c153fd0d3ae4f140dc967d2fd591ca920
Bruno Lovric <brunoolovric@gmail.com>
2021-03-12 09:02:26 +0100

    added strings project

---
+++ ex10/src/index.js
@@ -0,0 +1,17 @@
+function main(){
+
+    var myFirstName = "John";
+    var myLastName = "Johnson";
+    var myHomeTown = "Johntown";
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+
+}
+
+console.log(main());
+
+module.exports = main;
