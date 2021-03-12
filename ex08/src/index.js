9851ef2ef3cc5a2748640a8b0bf58ecb50104227
Bruno Lovric <brunoolovric@gmail.com>
2021-03-12 08:38:26 +0100

    added assignment operators project

---
+++ ex08/src/index.js
@@ -0,0 +1,21 @@
+function main(numOne, numTwo){
+
+    var myX = 12;
+    var myY = 19;
+    var myZ = 24;
+
+    myX += 3;
+    myY += 17;
+    myZ -= 20;
+
+    return {
+        myX,
+        myY,
+        myZ
+    };
+
+}
+
+console.log(main());
+
+module.exports = main;
